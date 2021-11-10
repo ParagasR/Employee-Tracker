05d15a0f937f7dbac45860a7225dff0bbe80c937
NinjaStep <r.paragas@outlook.com>
2021-11-09 21:58:08 -0700

    added schema and seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO departments (name)
+VALUES  ("Customer Service"),
+        ("Analytics"),
+        ("Management"),
+        ("Product Design");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES  ('Manager', 60000.00, NULL),
+        ('Project Manager', 90000.00, 4),
+        ('Sales Representative', 40000.00, 1),
+        ('Customer Representative', 20000.00, 1),
+        ('Data Analyst', 45000.00, 2),
+        ('Product Engineer', 75000.00, 4);
+
+INSERT INTO employees (first_Name, last_name, role_id, manager_id)
+VALUES  ('Beth', 'Smith', 1, NULL),
+        ('Rick', 'Sanchez', 2, NULL),
+        ('Morty', 'Smith', 6, 2),
+        ('Summer', 'Smith', 3, 1),
+        ('Jerry', 'Smith', 4, 1),
+        ('Mr.', 'Poopybutthole', 5, 2);
